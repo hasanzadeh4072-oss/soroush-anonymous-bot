728ac95bdfe20887c02b43c327284f5ace66efa1
hasanzadeh4072-oss <hasanzadeh4072@gmail.com>
2026-09-24 11:59:18 +0330

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,50 +1,8 @@
-const sendButton =
-    document.getElementById("sendButton");
+const sendButton = document.getElementById("sendButton");
+const status = document.getElementById("status");
 
-const status =
-    document.getElementById("status");
+alert("تست ۳ اجرا شد ✅");
 
-
-alert("TEST 3 - app.js جدید");
-
-
-sendButton.addEventListener("click", async () => {
-
-    sendButton.disabled = true;
-
-    status.textContent =
-        "در حال ارسال درخواست به Render...";
-
-
-    try {
-
-        await fetch(
-            "https://soroush-anonymous-bot.onrender.com/health",
-            {
-                method: "GET",
-                mode: "no-cors",
-                cache: "no-store"
-            }
-        );
-
-
-        status.textContent =
-            "درخواست به Render ارسال شد. ✅";
-
-
-    } catch (error) {
-
-        status.textContent =
-            "ارسال درخواست ناموفق بود. ❌\n\n" +
-            error.name +
-            "\n" +
-            error.message;
-
-    }
-
-
-    sendButton.disabled = false;
-
+sendButton.addEventListener("click", () => {
+    status.textContent = "تست ۳ اجرا شد ✅";
 });
-
-
